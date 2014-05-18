afda6f926b2684226d0166900e6013c77087aa49
Landon Manning <LManning17@gmail.com>
2014-05-17 23:34:30 -0300

    Fixed the spelling of "Colour" right up there b'y
    Fixed 'er right up there I did, yessir. Thanks ta Nautical Mile f'r the
    idea, eh?

--- canada.lua
+++ canada.lua
@@ -1,40 +1,53 @@
 -- http://i.imgur.com/RkLJFVh.jpg
 if love then
 	if love.filesystem then
-		love.filesystem.directoryEh	= love.filesystem.isDirectory
-		love.filesystem.fileEh		= love.filesystem.isFile
-		love.filesystem.fusedEh		= love.filesystem.isFused
+		love.filesystem.directoryEh			= love.filesystem.isDirectory
+		love.filesystem.fileEh				= love.filesystem.isFile
+		love.filesystem.fusedEh				= love.filesystem.isFused
 	end
 
 	if love.graphics then
-		love.graphcis.supportedEh	= love.graphics.isSupported
-		love.graphics.wireframeEh	= love.graphics.isWireframe
+		love.graphcis.supportedEh			= love.graphics.isSupported
+		love.graphics.wireframeEh			= love.graphics.isWireframe
+
+		love.graphics.getBackgroundColour	= love.graphics.getBackgroundColor
+		love.graphics.getColour				= love.graphics.getColor
+		love.graphics.getColourMask			= love.graphics.getColorMask
+		love.graphics.getColourMode			= love.graphics.getColorMode
+		love.graphics.setBackgroundColour	= love.graphics.setBackgroundColor
+		love.graphics.setColour				= love.graphics.setColor
+		love.graphics.setColourMask			= love.graphics.setColorMask
+		love.graphics.setColourMode			= love.graphics.setColorMode
 	end
 
 	if love.image then
-		love.image.compressedEh		= love.image.isCompressed
+		love.image.compressedEh				= love.image.isCompressed
 	end
 
 	if love.keyboard then
-		love.keyboard.downEh		= love.keyboard.isDown
-		love.keyboard.keyRepeatEh	= love.keyboard.hasKeyRepeat
-		love.keyboard.textInputEh	= love.keyboard.hasTextInput
+		love.keyboard.downEh				= love.keyboard.isDown
+		love.keyboard.keyRepeatEh			= love.keyboard.hasKeyRepeat
+		love.keyboard.textInputEh			= love.keyboard.hasTextInput
 	end
 
 	if love.math then
-		love.math.convexEh			= love.math.isConvex
+		love.math.convexEh					= love.math.isConvex
 	end
 
 	if love.mouse then
-		love.mouse.downEh			= love.mouse.isDown
-		love.mouse.grabbedEh		= love.mouse.isGrabbed
-		love.mouse.visibleEh		= love.mouse.isVisible
+		love.mouse.downEh					= love.mouse.isDown
+		love.mouse.grabbedEh				= love.mouse.isGrabbed
+		love.mouse.visibleEh				= love.mouse.isVisible
 	end
 
 	if love.window then
-		love.window.createdEh		= love.window.isCreated
-		love.window.visibleEh		= love.window.isVisible
-		love.window.focusEh			= love.window.hasFocus
-		love.window.mouseFocusEh	= love.window.hasMouseFocus
+		love.window.createdEh				= love.window.isCreated
+		love.window.visibleEh				= love.window.isVisible
+		love.window.focusEh					= love.window.hasFocus
+		love.window.mouseFocusEh			= love.window.hasMouseFocus
 	end
+end
+
+if love and love.graphics then
+
 end
